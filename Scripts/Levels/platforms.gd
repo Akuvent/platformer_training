b1f7b8dd242639d210569791514466b60bac1f18
Moi <alexis.rats2009@gmail.com>
2026-07-15 16:57:13 +0200

    Add moving grass cloud platforms
    Wire up Platforms scene with tile sprites, collision shape, and
    platforms.gd for horizontal AnimatableBody2D movement.

---
+++ Scripts/Levels/platforms.gd
@@ -0,0 +1,10 @@
+extends Node2D
+@onready var platform = $Platform
+var speed = 80
+var direction = -1
+
+
+
+func _physics_process(delta):
+	platform.velocity.x = speed * direction
+	platform.move_and_slide()
