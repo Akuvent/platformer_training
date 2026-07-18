67f0ccee2aae41ddd4911a00dccd2a624bd1ca27
Moi <alexis.rats2009@gmail.com>
2026-07-18 09:00:59 +0200

    fix(chanceblocks): spawn rewards as siblings with pop-up tween

--- Scripts/Entities/chanceblocks.gd
+++ Scripts/Entities/chanceblocks.gd
@@ -7,8 +7,15 @@ func hit_from_below():
 	var ennemy = ennemy_scene.instantiate()
 	var choice = randi_range(1,2)
 	if choice == 1:
-		add_child(coin)
+		get_parent().add_child(coin)
 		coin.global_position = global_position
+		var tween = create_tween()
+		tween.tween_property(coin, "position:y", coin.global_position.y - 16, 0.2)
 	if choice == 2:
-		add_child(ennemy)
+		get_parent().add_child(ennemy)
+		ennemy.set_physics_process(false)
 		ennemy.global_position = global_position
+		var tween = create_tween()
+		tween.finished.connect(func():
+			ennemy.set_physics_process(true))
+		tween.tween_property(ennemy, "position:y", ennemy.global_position.y - 16, 1)
