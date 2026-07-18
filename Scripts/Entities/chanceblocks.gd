ef98bdbd8c81e5d37d0d5629a32ecc15b6dd612e
Moi <alexis.rats2009@gmail.com>
2026-07-18 08:50:47 +0200

    Make chance blocks spawn a coin or enemy at random when hit

--- Scripts/Entities/chanceblocks.gd
+++ Scripts/Entities/chanceblocks.gd
@@ -1,6 +1,14 @@
 extends Node2D
-
-
+@export var coin_scene: PackedScene
+@export var ennemy_scene: PackedScene
 
 func hit_from_below():
-	pass
+	var coin = coin_scene.instantiate()
+	var ennemy = ennemy_scene.instantiate()
+	var choice = randi_range(1,2)
+	if choice == 1:
+		add_child(coin)
+		coin.global_position = global_position
+	if choice == 2:
+		add_child(ennemy)
+		ennemy.global_position = global_position
