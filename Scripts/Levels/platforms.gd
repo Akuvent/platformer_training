540f51f6b81bdae981f29692e49e9a320498f3dc
Moi <alexis.rats2009@gmail.com>
2026-07-24 13:29:00 +0200

    Add directional platform patrol and revise FirstLevel layout
    Support left/right ping-pong via dir, add a second platform, and move spawn into the platform section.

--- Scripts/Levels/platforms.gd
+++ Scripts/Levels/platforms.gd
@@ -4,8 +4,8 @@ extends Node2D
 #region Setup
 @onready var platform = $Platform
 @export var speed := 80.0
-@export var patrol_distance := 64.0 ## How far it travels from start_x
-
+@export var patrol_distance := 4 ## How far it travels from start_x
+@export var dir = true
 var direction := -1
 var start_x: float
 #endregion
@@ -17,9 +17,14 @@ func _ready():
 
 func _physics_process(delta):
 	platform.position.x += speed * direction * delta
-
+	if dir == true:
 	# Ping-pong between start and start + patrol_distance
-	if platform.position.x <= start_x:
-		direction = 1
-	elif platform.position.x >= start_x + patrol_distance:
-		direction = -1
+		if platform.position.x <= start_x:
+			direction = 1
+		elif platform.position.x >= start_x + patrol_distance * 16:
+			direction = -1
+	elif dir == false: 
+		if platform.position.x >= start_x:
+			direction = -1
+		elif platform.position.x <= start_x - patrol_distance * 16:
+			direction = 1
